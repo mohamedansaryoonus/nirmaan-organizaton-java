55da6de357a6b4d65b93cd9147e34e6bb4aa87ad
mohamedansaryoonus <ansaryoonus6763@gmail.com>
2025-05-03 11:54:01 +0530

    Add files via upload

---
+++ conditionalstatements.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class conditionalstatements {
+	
+	public static void main(String[]args)
+	{
+		int age=16;
+		if(age>=18) {
+			System.out.println("You are a adult.");
+		}else {
+		
+			if(age<=16) {
+				System.out.println("minor.");
+			}
+		}
+		
+	}
+
+}
