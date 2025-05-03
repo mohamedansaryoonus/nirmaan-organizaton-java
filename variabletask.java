5f9ef20b47ac75cc7b1267068cd4d964e7a941dd
mohamedansaryoonus <ansaryoonus6763@gmail.com>
2025-05-03 10:05:37 +0530

    day2

---
+++ variabletask.java
@@ -0,0 +1,38 @@
+package day2;
+
+public class variabletask {
+	
+	public static void main(String[]args)
+	{
+		byte num=100;
+		System.out.println(num);
+		
+		short s = 32000;
+		System.out.println(s);
+		
+		int mynum= 200;
+		System.out.println(mynum);
+		
+		long l=9636562814l;
+		System.out.println(l);
+		
+		float f= 25.12345678f;
+		System.out.println(f);
+		
+		double d  =25.12345678912;
+		System.out.println(d);
+		
+		char c = 'A';
+		System.out.println('A');
+		
+		boolean istrue= true;
+		System.out.println(true);
+		
+		String invitation= "ansar";
+		System.out.println("ansar");
+		
+		
+		
+	}
+
+}
